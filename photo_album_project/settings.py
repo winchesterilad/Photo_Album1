eb0deddf0bd919ab61a507eb2de3e1ea2e99297c
winchesterilad <winchester.ilad@evsu.edu.ph>
2026-05-24 22:35:00 +0800

    Updated settings.py

--- photo_album_project/settings.py
+++ photo_album_project/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 cloudinary.config(
     cloud_name=config('CLOUDINARY_CLOUD_NAME'),
     api_key=config('CLOUDINARY_API_KEY'),
-    api_secret=config('CLOUDINARY_API_SECRET')
+    api_secret=config('CLOUDINARY_API_SECRET'),
     secure=True
 )
 
